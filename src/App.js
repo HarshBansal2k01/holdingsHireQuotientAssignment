2d07310e53395a1907d10e8a2853185cbcccb66d
HarshBansal2k01 <ihbbro14@gmail.com>
2024-04-20 21:29:10 +0530

    done with fetching and bonus grouping part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [holdings, setHoldings] = useState([]);
+
+  useEffect(() => {
+    const fetchHoldings = async () => {
+      try {
+        const response = await axios.get(
+          "https://canopy-frontend-task.now.sh/api/holdings"
+        );
+        setHoldings(response.data.payload);
+      } catch (error) {
+        console.error("Error fetching holdings:", error);
+      }
+    };
+
+    fetchHoldings();
+  }, []);
+
+  const groupedHoldings = holdings.reduce((acc, holding) => {
+    const { asset_class } = holding;
+    if (!acc[asset_class]) {
+      acc[asset_class] = [];
+    }
+    acc[asset_class].push(holding);
+    return acc;
+  }, {});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="holdings-table-container">
+      <h1>Holdings Table</h1>
+      {Object.entries(groupedHoldings).map(([assetClass, holdings]) => (
+        <div key={assetClass}>
+          <h2>{assetClass}</h2>
+          <table className="holdings-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Ticker</th>
+                <th>Average Price</th>
+                <th>Market Price</th>
+                <th>Latest Change Percentage</th>
+                <th>Market Value (Base CCY)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {holdings.map((holding, index) => (
+                <tr key={index}>
+                  <td>{holding.name}</td>
+                  <td>{holding.ticker}</td>
+                  <td>{holding.avg_price}</td>
+                  <td>{holding.market_price}</td>
+                  <td>{holding.latest_chg_pct}</td>
+                  <td>{holding.market_value_ccy}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}{" "}
     </div>
   );
 }
